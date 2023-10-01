c809e7468a3eb49ded244f4c7e3b721864155068
Rahat Hussain <imcimc364@gmail.com>
2023-10-01 18:46:21 +0100

    Add frequency function added frequency function and completed the task and testing for it

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,14 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    if len(items) == 0:
+        pass
+    else:
+        frequencies.update({str(items[0]) : 1})
+        for i in range(1, len(items)):
+            if str(items[i]) in frequencies:
+                frequencies[str(items[i])] += 1
+            else:
+                frequencies.update({str(items[i]) : 1})
+
     return frequencies
